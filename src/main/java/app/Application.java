5d45b8e519230f638c30c36de3c895ede8a3b7a3
Ibrahim Coskun <Ibrahim.Coskun@my.fmi.unibuc.ro>
2019-04-06 20:12:39 +0300

    created app starting point

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+// We need the following in order to detect the filter
+// from the security package
+@ServletComponentScan(value="app.services")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
