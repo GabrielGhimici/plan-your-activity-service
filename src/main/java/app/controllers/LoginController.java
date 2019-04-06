05709a955773528e6d4b1b8ec0802411d7fc8ef1
Ibrahim Coskun <Ibrahim.Coskun@my.fmi.unibuc.ro>
2019-04-07 02:36:44 +0300

    added security to login service

--- src/main/java/app/controllers/LoginController.java
+++ src/main/java/app/controllers/LoginController.java
@@ -5,6 +5,7 @@ import app.dto.AuthDTO;
 import app.dto.ChangeDTO;
 import app.response.Login;
 import app.services.JwtFilter;
+import app.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,15 @@ public class LoginController {
     @Autowired
     private LoginDao loginDAO;
 
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping(value = "/login")
     public ResponseEntity<?> auth(@RequestBody AuthDTO auth) {
         Boolean correctCredentials = loginDAO.get(auth.getEmail(),auth.getPassword());
         if (correctCredentials) {
             Login jwtUser = new Login(auth.getEmail());
-            return ResponseEntity.ok("ok");
+            return ResponseEntity.ok(jwtService.getToken(jwtUser));
         }
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
@@ -36,5 +40,6 @@ public class LoginController {
         JwtFilter.logout(loginDAO.logout(request));
         return ResponseEntity.ok("OK");
     }
+
 
 }
