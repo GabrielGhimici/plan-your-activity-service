2df93d62478fdef571aa8920ba527b6282bc7d04
Ibrahim Coskun <Ibrahim.Coskun@my.fmi.unibuc.ro>
2019-04-07 00:48:40 +0300

    resolve junit dependencies problems

--- src/main/java/app/UnitTests.java
+++ src/main/java/app/UnitTests.java
@@ -1,17 +1,41 @@
 package app;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = Application.class)
 public class UnitTests {
 
     @LocalServerPort
     int randomServerPort;
 
-    
+/*  Just an example for the rest of the tests
+    @Test
+    public void testGetEmployeeListSuccess() throws URISyntaxException
+    {
+        RestTemplate restTemplate = new RestTemplate();
+
+        final String baseUrl = "http://localhost:" + randomServerPort + "/service/logout";
+        URI uri = new URI(baseUrl);
+
+        ResponseEntity<String> result = restTemplate.postForEntity(uri,null,String.class);
+
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getBody().contains("OK"));
+    }
+*/
 
 }
